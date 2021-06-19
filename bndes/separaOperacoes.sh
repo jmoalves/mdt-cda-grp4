95fab574430703f8b37386f336ead4fca672a7de
João Mauricio de O. Alves <jmoalves.dev@pobox.com>
2021-06-19 12:33:52 -0300

    Coloca header e verifica ano

--- bndes/separaOperacoes.sh
+++ bndes/separaOperacoes.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Header
+header=$( zcat operacoes-financiamento-operacoes-indiretas-automaticas.csv.gz | head -n 1 )
+
 # Datas de contratação - remove o cabeçalho
 anos=$(
 	zcat operacoes-financiamento-operacoes-indiretas-automaticas.csv.gz \
@@ -9,9 +12,25 @@ anos=$(
 		| tail -n +2
 	)
 
+# Cuidado! Pode acabar selecionando operações que não é do ano certo...
 for ano in $anos; do
+	file=$ano-operacoes-financiamento-operacoes-indiretas-automaticas.csv
+	echo $header > $file
 	zcat operacoes-financiamento-operacoes-indiretas-automaticas.csv.gz \
 		| grep $ano \
-		| gzip \
-		> $ano-operacoes-financiamento-operacoes-indiretas-automaticas.csv.gz
+		>> $file
+done
+
+echo
+
+# Checa se "deu ruim..."
+for ano in $anos; do
+	echo === $ano
+	file=$ano-operacoes-financiamento-operacoes-indiretas-automaticas.csv
+	cat $file \
+		| cut -d ';' -f 6 \
+		| sed 's/^[^0-9]*\([0-9]\+\)-.*/\1/g' \
+		| sort -u \
+		| tail -n +2 \
+	)
 done
