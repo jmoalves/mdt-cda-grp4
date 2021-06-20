36226500f45bd02fcf35edca6a86bf1c3133bc8d
João Mauricio de O. Alves <jmoalves.dev@pobox.com>
2021-06-20 00:11:46 -0300

    BNDES - Separa por ano

--- bndes/separaOperacoes.sh
+++ bndes/separaOperacoes.sh
@@ -2,10 +2,10 @@
 
 mkdir -p porAno
 
-# Header
+echo === Header
 header=$( zcat operacoes-financiamento-operacoes-indiretas-automaticas.csv.gz | head -n 1 )
 
-# Datas de contratação - remove o cabeçalho
+echo === Descobrindo anos
 anos=$(
 	zcat operacoes-financiamento-operacoes-indiretas-automaticas.csv.gz \
 		| cut -d ';' -f 6 \
@@ -13,23 +13,27 @@ anos=$(
 		| sort -u \
 		| tail -n +2
 	)
+echo Anos: $anos
 
-# Filtra por ano
 for ano in $anos; do
+	echo === Separa $ano
+
 	file=porAno/$ano-operacoes-financiamento-operacoes-indiretas-automaticas.csv
 	echo $header > $file
 	zcat operacoes-financiamento-operacoes-indiretas-automaticas.csv.gz \
-		| grep '".*";".*";".*";".*";".*";"$ano-' \
+		| grep "^.*;.*;.*;.*;.*;\"${ano}-" \
 		>> $file
+	gzip -9 $file
 done
 
-echo
 
 # Checa se "deu ruim..."
+echo
 for ano in $anos; do
-	echo === $ano
-	file=porAno/$ano-operacoes-financiamento-operacoes-indiretas-automaticas.csv
-	cat $file \
+	echo === Checa $ano
+
+	file=porAno/$ano-operacoes-financiamento-operacoes-indiretas-automaticas.csv.gz
+	zcat $file \
 		| cut -d ';' -f 6 \
 		| sed 's/^[^0-9]*\([0-9]\+\)-.*/\1/g' \
 		| sort -u \
