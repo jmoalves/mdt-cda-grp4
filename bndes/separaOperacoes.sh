add5a26d5caa1f8a48f2c8dcf6037a934f1c6e94
João Mauricio de O. Alves <jmoalves.dev@pobox.com>
2021-06-23 09:26:08 -0300

    BNDES - Separa por ano, removendo alguns campos

--- bndes/separaOperacoes.sh
+++ bndes/separaOperacoes.sh
@@ -15,6 +15,20 @@ anos=$(
 	)
 echo Anos: $anos
 
+## 3: "uf"
+## 4: "municipio"
+## 5: "municipio_codigo"
+## 6: "data_da_contratacao"
+## 7: "valor_da_operacao_em_reais"
+## 20: "setor_cnae"
+## 21: "subsetor_cnae_agrupado"
+## 22: "subsetor_cnae_codigo"
+## 23: "subsetor_cnae_nome"
+## 24: "setor_bndes"
+## 25: "subsetor_bndes"
+## 26: "porte_do_cliente"
+
+echo === Separando por ano - e cortando alguns campos
 for ano in $anos; do
 	echo === Separa $ano
 
@@ -22,19 +36,20 @@ for ano in $anos; do
 	echo $header > $file
 	zcat operacoes-financiamento-operacoes-indiretas-automaticas.csv.gz \
 		| grep "^.*;.*;.*;.*;.*;\"${ano}-" \
+		| cut -d ';' -f 3-7,20-26 \
 		>> $file
-	gzip -9 $file
+	gzip -f9 $file
 done
 
 
-# Checa se "deu ruim..."
+# Checa se "deu ruim..." - A data agora está em outro índice
 echo
 for ano in $anos; do
 	echo === Checa $ano
 
 	file=porAno/$ano-operacoes-financiamento-operacoes-indiretas-automaticas.csv.gz
 	zcat $file \
-		| cut -d ';' -f 6 \
+		| cut -d ';' -f 4 \
 		| sed 's/^[^0-9]*\([0-9]\+\)-.*/\1/g' \
 		| sort -u \
 		| tail -n +2
